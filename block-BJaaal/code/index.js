344b56a2a8d5f0ee9ddc4b33dd5b11dd30e7b7a1
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-04-15 15:13:28 +0530

    added backtick and double qotes and single quote usege file

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -4,6 +4,10 @@
 - Define a variable named `username` using `const` keyword and assign your name as a value.
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
 */
+const username = `Narasimhulu Vasam`
+let bio = `Learning to code`
+console.log (username)
+console.log(bio)
 
 let firstName = "Arya";
 let lastName = "Stark";
@@ -12,11 +16,16 @@ let lastName = "Stark";
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+alert("I am "+ username +" and my bio is "+bio)
+alert("I'm " + firstName +" "+ lastName + " of Winterfell")
+
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+alert(`I'am ${username} and my bio is ${bio}`)
+alert(`I'm ${firstName} ${lastName} of Winterfell`)
 
 let amount = 200;
 const taxPercent = 10;
@@ -27,6 +36,7 @@ const taxPercent = 10;
   `
   - Above message should be in two lines
 */
+alert("The total amount is  "+ amount + "\n  The tax amount is " + (amount * taxPercent) / 100 )
 
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
@@ -34,3 +44,4 @@ const taxPercent = 10;
   `
   - Above message should be in two lines
 */
+alert(`The total amount is ${amount} \n The Tax amount is ${(amount * taxPercent) / 100}`)
