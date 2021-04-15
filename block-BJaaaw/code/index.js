f2d2c67ff1ae2ed571eaf832de9f788114b04a4c
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-04-15 23:28:08 +0530

    added files

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -6,7 +6,7 @@
   - `if` the number is even print the message " number is even"
   - `if` the number is odd print the message "number is odd"
 */
-let num = prompt(Number("Enter the number"))
+let num = Number (prompt("Enter the number"))
 if(num % 2 == 0){
   alert("Number is even")
 }else{
@@ -17,10 +17,16 @@ if(num % 2 == 0){
 
   let num1 = Number(prompt("Enter first Number"))
   let num2 = Number(prompt("Enter second Number"))
-  alert(num1 >= num2 || num2 >= num1)
+  if(num1 > num2){
+    alert(`${num1} is greater`)
+  }else{
+    alert( `${num2} is greater`)
+  }
 
 // 3. Convert the above code using`?` terniary operator
 // num1 >= num2 || num2 >= num1 ? alert(num1 >= num2 ) : alert(num1 || num2)
+
+num1 > num2 ? alert( `${num1} is greater`) : alert( `${num2} is greater`)
 
 /*
 4. Write a program that asks the user for the house name and check the following conditions:
@@ -29,8 +35,8 @@ if(num % 2 == 0){
   - `if` house name is "lannister" then print the message " A lannister always pays his debt"
   - `else` print the message " All men must die"
 */
-let houseName = prompt("Enter the Data")
-if(houseName == "stark"){
+let houseName = prompt("Enter the housename")
+if(houseName === "stark"){
   alert("Winter is coming")
 }else if(houseName == "lannister"){
   alert("a lannister always pays his debt")
@@ -39,10 +45,53 @@ if(houseName == "stark"){
 }
 
 // 5. Convert the above code using`?` terniary operator
-  let  house = houseName == "stark"
+//  houseName === "stark" ? alert("winter is comming") : houseName === "lannister" ? alert("a lannister always pays his debt");
 // Switch
 
 // 6. Write a program that takes the number of the month(1 - 12) and alert number of days in the month.
+
+let monthname = prompt("Enter the name of the month")
+
+switch(monthname){
+  case "January":
+    alert("the number of days are 31");
+    break;
+    case "feb":
+    alert("the number of days are 28");
+    break;
+    case "mar":
+    alert("the number of days are 30");
+    break;
+    case "apr":
+    alert("the number of days are 31");
+    break;
+    case "may":
+    alert("the number of days are 30");
+    break;
+    case "june":
+    alert("the number of days are 31");
+    break;
+    case "july":
+    alert("the number of days are 30");
+    break;
+    case "aug":
+    alert("the number of days are 31");
+    break;
+    case "sep":
+    alert("the number of days are 30");
+    break;
+    case "oct":
+    alert("the number of days are 31");
+    break;
+    case "nov":
+    alert("the number of days are 31");
+    break;
+    case "dec":
+    alert("the number of days are 31");
+    break;
+    default:
+      alert("Not a valid input")
+}
 
 /* 7.
 - Write a program that take the salery of the user using prompt and alert the in -hand amount.You will find out the in -hand amount by deducting the tax amoun from salery.Conditions are given below.
@@ -52,6 +101,12 @@ if(houseName == "stark"){
 
 */
 
+let salary = prompt("Enter your salary")
+switch(salary){
+  case salary <=2000:
+    let tax = (salary * 10)/100;
+    alert( )
+}
 //  if..else vs switch
 
 /* 8.
@@ -64,7 +119,19 @@ Implement the condition give below using`if..else` and`switch` statement.
   - `marks > 0` alert`"Grade D"`
 
 */
-
+if(marks > 100){
+  alert("marks cant be greater than 100")
+}else if(marks > 80 && marks < 100){
+  alert("Grade A")
+}else if(marks > 80 && marks < 80){
+  alert('Grade B')
+}else if(marks > 50 && marks < 80){
+  alert("Grade C")
+}else if(marks > 30 && marks < 50){
+  alert("Grade C")
+}else{
+  alert("Grade D")
+}
 /* 9. Weather app
 
   - Ask user to provide the condition of the weather outside by asking `What is the weather like outside?`
