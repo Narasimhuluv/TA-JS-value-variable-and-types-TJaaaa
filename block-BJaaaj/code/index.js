32e5a108478a92ef90d3b86d0fee4a08598becce
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-04-15 12:51:43 +0530

    added files

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -6,29 +6,51 @@ let lastName = "Stark";
 // Do the following:
 
 // Alert the value of the firstName.
+alert(firstName)
 // Alert the value of the lastName.
+alert(lastName)
 // Assign a new value of `"John"` to firstName.
+let firstname = "John"
 // Assign a new value of `"Snow"` to lastName.
+let lastname = "Snow"
 // Alert the new value of `firstName` and `lastName` by concatenating them
+alert(firstname)
 // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
+const fullName = firstname +" "+ lastname
+console.log(fullName)
 
 // Declare three variables `name,` `newUser` and `isAdmin`.
 // Assign your name as the value of `name`.
 // Assign the value of `name` to `newUser`.
 // Set the value of `isAdmin` to `true`.
 // Alert the values of all the three variables.
+let name = "Narasimhulu"
+let newUser = "newUser"
+let isAdmin = "true"
+alert(name+ newUser+isAdmin)
+
+
+
 
 // Assignment and re-assignment
 
 var wiseMan = "Tyrion Lannister";
+console.log(wiseMan)
 
 // Follow instructions and do the following. In case of an error write the error in comment
 
 // Reassign the value of `wiseMan` to "Samwell Tarly"
+let wiseMan = "Samwell Tarly"  // Uncaught SyntaxError: Identifier 'wiseMan' has already been declared
 // Declare a variable `userName` with value "Lysa Arryn"
-// Declare a variable named `oddNumber` and assign a value `57`.
-// Reassign the value of `oddNumber` to 61
 
+let anotherUserName = "Lysa Arryn"
+console.log(anotherUserName)
+// Declare a variable named `oddNumber` and assign a value `57`.
+let oddNumber = 57
+console.log(oddNumber)
+// Reassign the value of `oddNumber` to 61
+let oddNumber = 61 // Uncaught SyntaxError: Identifier 'oddNumber' has already been declared
+console.log(oddNumber) 
 // Mathematical Operation
 
 let amount = 4280;
@@ -36,6 +58,14 @@ let amount = 4280;
 // Using mathematical operations find the solutions. `(+, -, \*, / , etc.)`
 
 // Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
+let addition = amount + 20
+console.log(addition)
 // Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
+let substraction = amount - 100
+console.log(substraction)
 // Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
+let multiplication = amount * 2
+console.log(multiplication)
 // Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
+let division = amount / 2
+console.log(division)
